6f84d582179016b75ae281a83d37bfa0a6d3a303
feulertovar <feuler@appcues.com>
2021-11-21 18:48:14 -0500

    first commit with terraform and ci configs

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "nasa-api-tf-state"
+    key     = "nasa-api/nasa-api.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+resource "aws_s3_bucket" "dev-apps-tf" {
+  bucket = "dev-apps-s3"
+  acl    = "private"
+}
