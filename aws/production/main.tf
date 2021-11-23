8e95b1991a9d3da8624c45d710a57e70d078d6a4
feulertovar <feuler@appcues.com>
2021-11-22 23:32:20 -0500

    testing monorepo 2

--- aws/production/main.tf
+++ aws/production/main.tf
@@ -10,15 +10,15 @@ terraform {
       version = "~> 3.63"
     }
   }
-  backend "s3" {
-    bucket  = "appcues-devsecops-tf-state"
+  backend "s3-prod" {
+    bucket  = "test-prod-tf-state"
     key     = "test-prod/test-prod.tfstate"
     region  = "us-west-2"
     encrypt = true
   }
 }
 
-resource "aws_s3_bucket" "appcues-devsecops-tf" {
-  bucket = "appcues-devsecops-test-prod-momo"
+resource "aws_s3_bucket" "test-prod-mono" {
+  bucket = "test-prod-mono"
   acl    = "private"
 }
--- aws/staging/main.tf
+++ aws/staging/main.tf
@@ -10,15 +10,15 @@ terraform {
       version = "~> 3.63"
     }
   }
-  backend "s3" {
-    bucket  = "appcues-devsecops-tf-state"
+  backend "s3-staging" {
+    bucket  = "test-staging-tf-state"
     key     = "test-staging/test-staging.tfstate"
     region  = "us-west-2"
     encrypt = true
   }
 }
 
-resource "aws_s3_bucket" "appcues-devsecops-tf" {
-  bucket = "appcues-devsecops-test-staging-momo"
+resource "aws_s3_bucket" "test-staging-mono" {
+  bucket = "test-staging-mono"
   acl    = "private"
 }
